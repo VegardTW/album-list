604862e3cc4dcf3926add2549617cdeac6f37a14
WallardW <vegard.wik@gmail.com>
2023-03-03 20:12:31 +0100

    Oppdatert responsivitet, ting funker.

--- album-div-top-offsets-shuffle.js
+++ album-div-top-offsets-shuffle.js
@@ -11,7 +11,7 @@ shuffleArray(indices);
 // loop through each album div and set its top style using the randomized indices
 for (let i = 0; i < albums.length; i++) {
     const album = albums[indices[i]];
-    album.style.top = ((i + 1) * 200) + 'px';
+    album.style.top = ((i + 1) * 23) + 'vw';
 }
 
 // helper function to shuffle an array randomly
@@ -22,3 +22,4 @@ function shuffleArray(array) {
     }
     return array;
 }
+
--- random-pos.js
+++ random-pos.js
@@ -6,16 +6,90 @@ function random() {
     return val
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-//const container = document.querySelector('.album-container');
-    const albums = document.querySelectorAll('.album');
+const albums = document.querySelectorAll('.album');
 
-    albums.forEach(album => {
-        const x = random() * 3;
-        const y = random() * 10;
-        const z = random();
-        album.style.left = `${parseInt(album.style.left || 36) + x}vw`;
-        album.style.top = `${parseInt(album.style.top || 30) + y}px`;
-        album.style.zIndex = `${parseInt(album.style.zIndex || 0) + z}`;
-    });
+enquire.register("screen and (min-width:1000px)", {
+    match: function () {
+
+        // create an array of randomized indices for the albums array
+        const indices = [];
+        for (let i = 0; i < albums.length; i++) {
+            indices.push(i);
+        }
+        shuffleArray(indices);
+
+        // loop through each album div and set its top style using the randomized indices
+        for (let i = 0; i < albums.length; i++) {
+            const album = albums[indices[i]];
+            album.style.top = ((i + 1) * 200) + 'px';
+        }
+
+        // helper function to shuffle an array randomly
+        function shuffleArray(array) {
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+            return array;
+        }
+
+        albums.forEach(album => {
+            const x = random() * 3;
+            const y = random() * 10;
+            const z = random();
+            album.style.left = `${parseInt(album.style.left || 40) + x}vw`;
+            album.style.top = `${parseInt(album.style.top || 30) + y}px`;
+            album.style.zIndex = `${parseInt(album.style.zIndex || 0) + z}`;
+        })
+        let largestTopValue = -Infinity;
+        albums.forEach((album) => {
+            const topValue = parseInt(album.style.top);
+            if (topValue > largestTopValue) {
+                largestTopValue = topValue;
+            }
+        });
+       /* const newHeightValue = largestTopValue + 200;
+        console.log(largestTopValue)
+
+        const mainElement = document.querySelectorAll(".main");
+        console.log(newHeightValue)
+        mainElement.style.height = newHeightValue + "px";*/
+
+    }
+})
+
+enquire.register("screen and (max-width:1000px)", {
+    match: function () {
+
+        // create an array of randomized indices for the albums array
+        const indices = [];
+        for (let i = 0; i < albums.length; i++) {
+            indices.push(i);
+        }
+        shuffleArray(indices);
+
+        // loop through each album div and set its top style using the randomized indices
+        for (let i = 0; i < albums.length; i++) {
+            const album = albums[indices[i]];
+            album.style.top = ((i + 1) * 23) + 'vw';
+        }
+
+        // helper function to shuffle an array randomly
+        function shuffleArray(array) {
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+            return array;
+        }
+
+        albums.forEach(album => {
+            const x = random() * 3;
+            const y = random() * 1.1;
+            const z = random();
+            album.style.left = `${parseInt(album.style.left || 30) + x}vw`;
+            album.style.top = `${parseInt(album.style.top || 30) + y}vw`;
+            album.style.zIndex = `${parseInt(album.style.zIndex || 0) + z}`;
+        })
+    }
 });

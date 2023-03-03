5fa567d2383fcecb342f9ef35e19fe6b5629b963
WallardW <vegard.wik@gmail.com>
2023-03-04 00:12:20 +0100

    alle button-states fungerer

--- buttons.js
+++ buttons.js
@@ -1,16 +1,24 @@
 document.getElementById("go-to-top");
 document.getElementById("refresh");
+const button = document.getElementById("menu-icon");
 
-function buttons() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+// function buttons() {
+//     document.body.scrollTop = 0; // For Safari
+//     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 
 function reload() {
     location.reload()
 }
 
-function goToX() {
-    var element = document.getElementById("menu-icon");
-    element.classList.toggle("animate");
+function allButtons() {
+    if (button.classList.contains("go-to-top")) {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    } else if (button.classList.contains("menu")) {
+        button.classList.toggle("menu")
+        button.classList.toggle("cross")
+    } else if (button.classList.contains("cross")) {
+        button.classList.toggle("menu")
+        button.classList.toggle("cross")
+    }
 }
--- scroll-detection-v2.js
+++ scroll-detection-v2.js
@@ -1,11 +1,15 @@
 let isScrollTop = true
 const element = document.getElementById("menu-icon");
 window.addEventListener('scroll', function () {
-    if (window.scrollY > 20 && isScrollTop !== false) {
+    if (window.scrollY > 180 && isScrollTop !== false) {
         isScrollTop = false
-        element.classList.toggle("animate")
-    } else if (window.scrollY < 20 && isScrollTop !== true) {
+        element.classList.toggle("go-to-top")
+        element.classList.toggle("menu")
+        element.classList.remove("cross")
+     } else if (window.scrollY < 180 && isScrollTop !== true) {
         isScrollTop = true
-        element.classList.toggle("animate")
+        element.classList.toggle("go-to-top")
+        element.classList.toggle("menu")
+        element.classList.remove("cross")
     }
 });
